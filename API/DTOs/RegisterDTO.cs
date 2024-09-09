392e9a219bf37fba26ddaaf1a20b8bcc9255574f
Kenia Kennedy <keniakennedy3@gmail.com>
2024-09-09 16:08:48 -0400

    End of section 7

--- API/DTOs/RegisterDTO.cs
+++ API/DTOs/RegisterDTO.cs
@@ -6,9 +6,9 @@ namespace API.DTOs;
 public class RegisterDTO
 {
     [Required]
-    [MaxLength(100)]
-    public required string Username { get; set; }
+    public string Username { get; set; } = string.Empty;
     
     [Required]
-    public required string password { get; set; }
+    [StringLength(8, MinimumLength = 4)]
+    public string password { get; set; } = string.Empty;
 }
